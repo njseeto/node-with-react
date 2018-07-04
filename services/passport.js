c2d61debf4eb455a533ee84e16ff7be758e8033e
Natalie <njseeto@users.noreply.github.com>
2018-07-04 18:01:09 +0100

    feat(cookies): add cookie-session library and enable cookies

--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,17 @@ const keys = require('../config/keys')
 
 const User = mongoose.model('users') //one argument means we are trying to fetch something from the collection
 
+passport.serializeUser((user, done) => { // done is a callback
+    done(null, user.id) // user.id refers to the user model instance stored in the mongoDB collection
+})
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then(user => {
+            done(null, user)
+        })
+})
+
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
