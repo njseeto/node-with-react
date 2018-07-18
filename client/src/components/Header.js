a79b0ad8c60b279145ea2c214e0f0be0577ffa81
Natalie Seeto <natalie.seeto@Fandral.local>
2018-07-18 15:49:13 +0100

    style(container): add container class for styling..

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,9 +5,7 @@ class Header extends Component {
         return(
             <nav>
                 <div className="nav-wrapper">
-                    <a className="left-brand-logo">
-                        Emaily
-                    </a>
+                    <a className="left-brand-logo">Emaily</a>
                     <ul className="right">
                         <li>Login with Google</li>
                     </ul>
