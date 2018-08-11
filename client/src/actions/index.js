f721e534e1f7bfbc358c0b8cb211450a106ab52d
Natalie <njseeto@users.noreply.github.com>
2018-08-11 11:20:12 +0100

    feat(stripe): stripe token post request

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -5,3 +5,8 @@ export const fetchUser = () => async dispatch => {
     const res = await axios.get('/api/currentuser')
     dispatch({ type: FETCH_USER, payload: res.data }) //only dispatch the response once we have the action
 }
+
+export const handleToken = token => async dispatch => {
+    const res = await axios.post('/api/stripe', token)
+    dispatch({ type: FETCH_USER, payload: res.data }) 
+}
